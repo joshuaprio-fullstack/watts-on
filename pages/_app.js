f82848fa1c29dfa9ad1719e02434e4755b1febda
Josh <joshuaprio@fullstack.ph>
2022-11-02 14:32:54 +0800

    Fix issue with preview

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { repositoryName } from '../prismicio'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  <script async defer src="https://static.cdn.prismic.io/prismic.js?new=true&repo=watts-on"></script>
   return (
     <PrismicProvider
       internalLinkComponent={({ href, ...props }) => (
